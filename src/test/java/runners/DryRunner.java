3cf8f955845b9e7dc5548d35d5f38cbb3127cadf
Hayk C <haykchilingaryan1@gmail.com>
2023-09-14 17:56:33 -0500

    TestCase2

--- src/test/java/runners/DryRunner.java
+++ src/test/java/runners/DryRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features="./src/test/resources/features",
 		glue="tests",
-		tags="@SalesandExpensesHeader",
+		tags="@DashboardElementsVerification",
 		dryRun=true
 		)
 public class DryRunner {
